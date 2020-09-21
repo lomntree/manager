89e052795fc092c0beb77c6033f49f7cc0cb8a66
lomntree <2441089012@qq.com>
2020-09-21 11:20:45 +0800

    2020/9/21

--- mall-rest/src/main/java/org/ljw/rest/service/TbContentService.java
+++ mall-rest/src/main/java/org/ljw/rest/service/TbContentService.java
@@ -3,5 +3,5 @@ package org.ljw.rest.service;
 import org.ljw.utils.FjnyResult;
 
 public interface TbContentService {
-	FjnyResult getTbContentList(Long cid);
+	public FjnyResult getTbContentList(Long cid);
 }
